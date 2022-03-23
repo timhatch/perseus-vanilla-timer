5c7f8c99a2367194ab3256cd1ba7daa37e59f75b
Tim Hatch <hatch.tim@gmail.com>
2022-03-23 13:07:33 +0000

    Create timer-countdown
    Create a CountdownTimer class

--- timer-countdown.js
+++ timer-countdown.js
@@ -1,27 +1,4 @@
-//  A javascript timer for WC-format competitions.
-//  Uses WebAudio to generate a standardised audio signals without latency effects
-//  Copyright 2012-22  T J Hatch
-//
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-
-// NOTE
-// Older browsers may not support es6 in which case this code will need to be transpiled to es5
-// Support for `toLocaleString` is not required/assumed
-
-/*
-* COUNTDOWN TIMER IMPLEMENTATION
-* Implement a user-controlled countdown timer
-*/
+import TimerView from './timer-baseclass.js'
 
 class CountdownTimer extends TimerView {
   // Constructor
@@ -56,3 +33,4 @@ class CountdownTimer extends TimerView {
   }
 }
 
+export default CountdownTimer
