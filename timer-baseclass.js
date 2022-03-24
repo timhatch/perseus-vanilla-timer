6689cc08355674854bbd7578ffe5b24278c6471b
Tim Hatch <hatch.tim@gmail.com>
2022-03-24 11:03:17 +0000

    Refactor

--- timer-baseclass.js
+++ timer-baseclass.js
@@ -1,15 +1,6 @@
 import AudioSignal, {AudioCTX} from "./audiosignal.js"
 
-// Utility method to return the remaining time in the current period
-const toString = (time) => {
-    const m = Math.floor(time / 60)
-    let   s = time % 60
-
-    // return (m + ':' + s.toLocaleString('en-US', { minimumIntegerDigits: 2 }))
-    // NOTE: Some browsers don't yet support toLocaleString, to format manually
-    if (s < 10) s = '0' + s
-    return `${m}:${s}`
-}
+import {format, display}       from "./timer-views.js"
 
 /*
 * BASE TIMER IMPLEMENTATION
@@ -35,11 +26,6 @@ class TimerView {
 
     // If the WebAudio API is supported, create the required audio signals
     this.audio = AudioCTX ? [425, 600].map((f) => new AudioSignal(f)) : null
-    // Scale the displayed text to the screen dimensions
-    const viewportHeight     = document.documentElement.clientHeight
-    this.el                  = document.getElementById('inner')
-    this.el.style.fontSize   = (0.7 * viewportHeight)+'px'
-
   }
 
   // Given a time in seconds, compare it with the stored time remaining on the clock and if
@@ -50,17 +36,19 @@ class TimerView {
   run(timestamp) {
     if (Math.floor(timestamp) !== Math.floor(this.model.remaining)) {
       this.model.remaining = timestamp
-      this.updateClock()
-      this.audio && this.playSound()
       this.broadcast()
+      this.playSound()
     }
   }
 
   // Play audio signals (650ms for the rotation start/end and one minute warning, 325ms for the five
   // second countdown
   playSound() {
-    const t = this.remainingTime()
-    if (t === 0 || t === 60 || t === this.model.rotation) this.audio[1].play(650)
+    if (!this.audio) return
+
+    const r = display(this.model)
+    const t = Math.floor(r)
+    if (t === 0 || t === 60 || t === this.model.climbing) this.audio[1].play(650)
     if (t < 6 && t > 0) this.audio[0].play(325)
   }
 
@@ -70,31 +58,6 @@ class TimerView {
   broadcast() {
     const data = JSON.stringify(this.model)
     this.socket.send(data)
-  }
-
-  // Update the time display & play any relevant audible signal
-  // this implementation assume that upDateClock is called only where time remaining
-  // *in seconds* changes
-  updateClock() {
-    const t = Math.floor(this.model.remaining) ? this.remainingTime() : this.model.climbing
-
-    this.el.textContent = toString(t)
-  }
-
-  remainingTime() {
-    const t = this.model.remaining > this.model.preparation
-                ? this.model.remaining - this.model.preparation 
-                : this.model.remaining
-    return Math.floor(t)
-  }
-
-  // Send a message to reset the clock
-  reset() {
-    const data   = Date.now()
-    const client = new XMLHttpRequest()
-    client.open("POST", '/timers/reset', true)
-    client.setRequestHeader('Content-Type', 'application/json')
-    client.send(data)
   }
 }
 
---
+++ timer-views.js
@@ -0,0 +1,50 @@
+// Utility method to return the remaining time in the current period
+// Format the time into a mm:ss string
+//
+// sig: (Double time) -> String
+export function format(time) {
+  const m = Math.floor(time / 60)
+  const s = Math.floor(time % 60)
+
+  return (m + ':' + s.toLocaleString('en-US', {minimumIntegerDigits: 2}))
+}
+
+// Return the time to be displayed
+// Assumes the following IFSC time periods are provided:
+// climbing:    the allowed climbing period
+// preparation: the allowed interval following a climbing period (e.g. in boulder, the transition time)
+// remaining:   the calculated time  remaining within the rotation/attempt period
+//
+// sig: (Hash) -> Double
+export function display({remaining, climbing, preparation}) {
+  // If there's no time left in the rotation/attempt period, return the climbing time
+  if (!Math.floor(remaining)) return climbing
+
+  // Otherwise. calculate whether we're in the climbing or preparation period and return the
+  // remaining time within the relevant period
+  return remaining > preparation ? (remaining - preparation) : remaining
+}
+
+// Fullscreen
+export async function fullscreen() {
+  const page = document.documentElement
+  if (page.requestFullscreen)       return page.requestFullscreen()
+  if (page.webkitRequestFullscreen) return page.webkitRequestFullscreen()
+}
+
+// Resize
+export function onResize(el) {
+  el.style.fontSize = `${document.documentElement.clientHeight * 0.7}px`
+}
+
+// Display time
+export function clockTime(el) {
+  return (event) => {
+    const opts     = JSON.parse(event.data) // Parse the `data` string
+    const time     = display({...opts})     // Calculate/format the time to be displayed
+    el.textContent = format(time)           // Display the time
+    // CHECK VALUES
+    console.log(opts)
+  }
+}
+
