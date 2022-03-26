e3f0e48734f2587f8aa1de60a255485a50bc4fc3
Tim Hatch <hatch.tim@gmail.com>
2022-03-26 10:38:56 +0000

    Update timer-countdown.js
    Add a method to reset the clock

--- timer-countdown.js
+++ timer-countdown.js
@@ -14,21 +14,13 @@ class CountdownTimer extends BaseTimer {
   constructor(options) {
     // Instantiate the timer
     super(options)
-    
+
+    this.model.rotation = this.model.climbing + this.model.preparation
+    this.model.end      = this.model.start
+
     // Bind and run the clock
     this.clock = this.run.bind(this)
-    this.clock(null)
-
-    // TODO: Refactor to use ws
-    const es     = new EventSource('/timers/reset')
-    es.onmessage = this.handleESMessage.bind(this)
-  }
-
-  // TODO: Refactor to use ws
-  // React to any Server Sent Event message fired by the server
-  handleESMessage(eventsource) {
-    const now      = parseFloat(eventsource.data)       // (float) milliseconds
-    this.model.end = now + ((this.model.end - now) > 0 ? 0 : (this.model.rotation * 1000) + 899)
+    this.clock(0)
   }
   
   // Override the `run` method from the BaseTimer class, looping using requestAnimationFrame
@@ -42,6 +34,26 @@ class CountdownTimer extends BaseTimer {
     // Use requestAnimationFrame() in preference to setTimeout(). requestAnimationFrame notionally runs locked
     // to the screen refresh rate. Running faster than this is unnecessary.
     requestAnimationFrame(this.clock)
+  }
+
+  // Handle clock interrupts
+  reset() {
+    const offset     = 1000                         // Move actions 1s into the future (Human Factors)
+    const reset_abs  = offset + Date.now()          // Actual time at which reset was initiated
+    const reset_rel  = offset + performance.now()   // Time Offset at which reset was initiated
+
+
+    // If time remained on the clock when the reset occurred, zero the clock (by setting the remaining
+    // time to zero. Otherwise the new end time will be equal to `this.model.rotation` milliseconds
+    const new_end    = (this.model.end - reset_abs) > 0 ? 0 : (this.model.rotation * 1000) 
+
+    // Reset start and end times for the clock.
+    // Set the new start time relative to the clock used by requestAnimationFrame by subtracting the
+    // "relative time" (i.e. the time reference used by requestAnimationFrame) from "real time" when
+    // the reset occurred.
+    // The end time is set in "true" time
+    this.model.start = reset_abs - reset_rel
+    this.model.end   = reset_abs + new_end
   }
 }
 
